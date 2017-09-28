088323d13c93127112f83e9c3089f114006b7fcf
jtruong2 <jtruong2@comcast.net>
2017-09-28 11:33:28 -0600

    sessions else redirect path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
       redirect_to user
     else
       flash[:error] = "Failed to login. Try again."
-      render 'new'
+      redirect_to root_path
     end
   end
 
