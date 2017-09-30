36b50476383a9117809efdff0bd3e0f8f2ac7ca4
jtruong2 <jtruong2@comcast.net>
2017-09-30 12:09:42 -0600

    removed tmp in public

--- app/services/qr_code.rb
+++ app/services/qr_code.rb
@@ -5,6 +5,6 @@ class QrCode
     qrcode = RQRCode::QRCode.new(url)
     png = qrcode.as_png(resize_gte_to: false,resize_exactly_to: false, fill: 'white', color: 'black', size: 120, border_modules: 4, module_px_size: 6, file: nil)
     str = png.to_s.force_encoding("UTF-8")
-    IO.write("public/tmp/qrcode.png", str)
+    IO.write("public/qrcode.png", str)
   end
 end
