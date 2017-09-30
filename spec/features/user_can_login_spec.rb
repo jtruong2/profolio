4338ad92cbdb1922db295a63853c2fc059557c3c
jtruong2 <jtruong2@comcast.net>
2017-09-29 20:18:03 -0600

    fix assets compile

--- spec/features/user_can_login_spec.rb
+++ spec/features/user_can_login_spec.rb
@@ -11,16 +11,4 @@ RSpec.describe "User logs in" do
     expect(page).to have_content("Welcome back, #{user.first_name}")
     expect(User.all.count).to eq(1)
   end
-
-  it "logs in with linkedin" do
-    stub_linkedin
-
-    visit root_path
-    within('.linkedin') do
-      click_link
-    end
-
-    expect(page).to have_content("Welcome back, Jimmy")
-    expect(User.all.count).to eq(1)
-  end
 end
