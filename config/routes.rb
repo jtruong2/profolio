978a11dd5b2644f7ab30196fb1af11a2eb796b28
jtruong2 <jtruong2@comcast.net>
2017-09-29 23:43:13 -0600

    fixes heroku destroy methods

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,10 @@ Rails.application.routes.draw do
     get '/profile/new', to: 'profile#new'
     get '/profiles', to: 'profile#index'
     post '/profiles', to: 'profile#create'
-    delete '/profiles/:id', as: 'delete_profile', to: 'profile#destroy'
+    get '/profiles/:id', as: 'delete_profile', to: 'profile#destroy'
   end
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
+  get '/logout', to: 'sessions#destroy'
 end
